005e4581935ba014981c754d7ddd09b16aecdf5d
Dao Huy Hoang <daohuyhoang87@gmail.com>
2017-05-25 16:44:17 +0700

    Update  script

--- NGINX-LB/scripts/nginx-stream-status.sh
+++ NGINX-LB/scripts/nginx-stream-status.sh
@@ -13,6 +13,9 @@ cd /opt/source/nginx-1.11.10
 
  ./configure --user=nginx --group=nginx --add-module=../nginx-module-sts/ --add-module=../nginx-module-vts/ --add-module=../nginx-module-stream-sts/ --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-file-aio --with-stream --with-http_geoip_module
 
+ make
+ make install
+ 
 useradd -r nginx
 mkdir -p /var/cache/nginx/client_temp/
 chown nginx. /var/cache/nginx/client_temp/
